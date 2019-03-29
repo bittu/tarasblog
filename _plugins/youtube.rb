49ca07cf532792641f0a38a522a7e13d47d586a6
Sandeep Vemula <sandeep.kumar-vemula@bt.com>
2019-03-29 21:21:32 +0000

    updated

--- _plugins/youtube.rb
+++ _plugins/youtube.rb
@@ -31,7 +31,7 @@ module Jekyll
         # Thanks to Andrew Clark for the inline CSS calculation idea <http://contentioninvain.com/2013/02/13/video-embeds-for-responsive-designs/>
         intrinsic = ((@height.to_f / @width.to_f) * 100)
         padding_bottom = ("%.2f" % intrinsic).to_s  + "%"
-        %Q{<div class="embed-responsive"><iframe src="//www.youtube-nocookie.com/embed/#{@videoid}?rel=0" frameborder="0" allowfullscreen></iframe></div>}
+        %Q{<div class="embed-responsive embed-responsive-16by9"><iframe src="//www.youtube-nocookie.com/embed/#{@videoid}?rel=0" frameborder="0" allowfullscreen></iframe></div>}
       end
     end
   end
